fd75bb94c4a2aa941dac259c0087732cc301f2c5
quang <102quangnn@gmail.com>
2023-03-10 17:09:42 +0700

    dfgngfglsdg

--- build/layer2.sh
+++ build/layer2.sh
@@ -44,7 +44,6 @@ EOF
 cat > /etc/sysctl.d/kubernetes.conf << EOF
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
-net.bridge.bridge-nf-call-arptables = 1
 net.ipv4.ip_forward = 1
 EOF
 
